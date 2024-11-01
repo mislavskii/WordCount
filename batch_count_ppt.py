2a639d0272e6dd62e58f4e7078a2486293f0195a
mislavskii <jornjat@gmail.com>
2024-11-01 17:36:47 +0700

    vcs enabled

---
+++ batch_count_ppt.py
@@ -0,0 +1,42 @@
+import os
+from pprint import pprint
+
+from word_counter_ppt import WordCounter as WC
+path = r'F:\User\Earn\Translate\Eng\Stories\Value\Processed'
+
+all_files = sorted(os.listdir(path))
+pprint(all_files)
+print()
+
+source_files = sorted([path + '/' + file for file in all_files if 'eng.' not in file.lower()],
+                      key=lambda x: x.split('.')[-2][-1])
+out_files = sorted([path + '/' + file for file in all_files if 'eng.' in file.lower()],
+                   key=lambda x: x.split('.')[-2].replace('_', ' ').rstrip(' ENG')[-1])
+
+
+total_count = 0
+for in_file, out_file in zip(source_files, out_files):
+    print(in_file, out_file, sep='\n')
+    in_eng_count, out_eng_count = WC(in_file), WC(out_file)
+    in_eng_count.count_words()
+    out_eng_count.count_words()
+    print(in_eng_count.en, out_eng_count.en, sep='\n')
+    count = out_eng_count.en_count - in_eng_count.en_count
+    print(count, end='\n\n')
+    total_count += count
+
+
+print(total_count * 1.3)
+
+
+"""word_counts = 0
+for file in files:
+    try:
+        print(file + ':', end=' ')
+        words = count(file)
+        print(words)
+        word_counts += count(file)
+        files.remove(file)
+    except:
+        print('Processing error')
+        continue"""
